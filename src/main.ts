0f2a6d57bc691c4b7fec5817204c94fb2badc1c1
charlie <cfbarrons>
2025-10-23 17:14:43 -0600

    m ]\

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+let canvas = document.querySelector<HTMLCanvasElement>("#main-game")!;
+let ctx = canvas.getContext("2d")!;
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+ctx.fillStyle = "purple"
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function onResize(){
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+console.log("resized window", canvas.width, canvas.height)
+
+}
+
+window.addEventListener("resized window" onResize)
+
+
+
+
