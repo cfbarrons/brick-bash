4551f4524d6e1bf58d50ee65dc5731cc82b10f64
charlie <cfbarrons>
2025-10-24 16:09:08 -0600

    git

---
+++ src/canvas-ctx.ts
@@ -0,0 +1,19 @@
+let canvas = document.querySelector<HTMLCanvasElement>("#main-game")!;
+
+let ctx = canvas.getContext("2d")!;
+
+function initCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  window.addEventListener("resize", onResize);
+}
+
+function onResize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  console.log("Resized window", canvas.width, canvas.height);
+}
+
+export { canvas, ctx, initCanvas };
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,7 @@
-import './style.css'
+import { ctx, canvas, initCanvas } from "./canvas-ctx";
+import "./style.css";
 
+initCanvas();
 
-
-let canvas = document.querySelector<HTMLCanvasElement>("#main-game")!;
-let ctx = canvas.getContext("2d")!;
-
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-ctx.fillStyle = "purple"
+ctx.fillStyle = "purple";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-function onResize(){
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-console.log("resized window", canvas.width, canvas.height)
-
-}
-
-window.addEventListener("resized window" onResize)
-
-
-
-
